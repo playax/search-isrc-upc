f842b573fc1ec00cc2ccfc30c5c3ad03dbe4faf0
Talytta Carvalho <talytta110@hotmail.com>
2023-06-09 14:47:12 -0300

    Remove logs

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -10,11 +10,7 @@ exports.handler = async function () {
     body: new URLSearchParams({ grant_type: "client_credentials" }),
   };
 
-  console.log({options})
-
   const response = await (await fetch("https://accounts.spotify.com/api/token", options)).json()
-
-  console.log({response})
 
   return {
     statusCode: 200,
