e535770a7b7137245a7979f32de3b5fba27a0d45
Talytta Carvalho <talytta110@hotmail.com>
2023-06-09 14:37:24 -0300

    Fix netlify function

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,8 +1,13 @@
 exports.handler = async function (event, context) {
 
+
+
   return {
-    "access_token": "BQBqEO_hmP_M_5SymUBfL_Zr7Gs-AXfRNS8dtZK47By5oPSHbrwdCnmKaiShjDST2jn8JymDQwORAyOeK1_aq7-uk9zJAByaHYZrRewqZfdzoPef33w",
-    "token_type": "Bearer",
-    "expires_in": 3600
-  }
+    statusCode: 200,
+    body: JSON.stringify({
+      "access_token": "BQBqEO_hmP_M_5SymUBfL_Zr7Gs-AXfRNS8dtZK47By5oPSHbrwdCnmKaiShjDST2jn8JymDQwORAyOeK1_aq7-uk9zJAByaHYZrRewqZfdzoPef33w",
+      "token_type": "Bearer",
+      "expires_in": 3600
+    }),
+  };
 };
