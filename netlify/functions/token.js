e8e0ed26ce80b84957b93a77cc3b02919c5d9d49
Talytta Carvalho <talytta110@hotmail.com>
2023-06-09 14:31:02 -0300

    Create a serach page

---
+++ netlify/functions/token.js
@@ -0,0 +1,8 @@
+exports.handler = async function (event, context) {
+
+  return {
+    "access_token": "BQBqEO_hmP_M_5SymUBfL_Zr7Gs-AXfRNS8dtZK47By5oPSHbrwdCnmKaiShjDST2jn8JymDQwORAyOeK1_aq7-uk9zJAByaHYZrRewqZfdzoPef33w",
+    "token_type": "Bearer",
+    "expires_in": 3600
+  }
+};
